1614f5517cd5942ad01849142b2797887626c2a7
Aleks <z.sobieraj.spam@gmail.com>
2018-05-27 08:58:01 -0400

    Route transitions

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -8,19 +8,28 @@ import RestrictedRoute from './RestrictedRoute'
 import { Route, Switch } from 'react-router-dom'
 import UserProfile from './UserProfile'
 import ViewTeam from './ViewTeam'
+import ReactCSSTransitionReplace from 'react-css-transition-replace'
 
 class Main extends React.Component {
 	render() {
 		return (
 			<div>
 				<ConnectNavigationBar></ConnectNavigationBar>
-				<Switch>
-					<Route path="/" exact render={ () => (<Login {...this.props} />) }/>
-					<RestrictedRoute {...this.props} authed={this.props.user.token.length !== 0} path='/teams' component={ViewTeam} />
-					<RestrictedRoute {...this.props} authed={this.props.user.token.length !== 0} path='/user/edit' component={EditProfile} />
-					<RestrictedRoute {...this.props} authed={this.props.user.token.length !== 0} path='/user' component={UserProfile} />
-					<Route component={NotFound} />
-				</Switch>
+				<ReactCSSTransitionReplace 
+					transitionName="fade" 
+					transitionEnterTimeout={500} 
+					transitionLeaveTimeout={300}
+				>
+					<div key={this.props.routing.location.pathname}>
+						<Switch location={this.props.routing.location}>
+							<Route path="/" exact render={ () => (<Login {...this.props} />) }/>
+							<RestrictedRoute {...this.props} authed={this.props.user.token.length !== 0} path='/teams' component={ViewTeam} />
+							<RestrictedRoute {...this.props} authed={this.props.user.token.length !== 0} path='/user/edit' component={EditProfile} />
+							<RestrictedRoute {...this.props} authed={this.props.user.token.length !== 0} path='/user' component={UserProfile} />
+							<Route component={NotFound} />
+						</Switch>
+					</div>
+				</ReactCSSTransitionReplace>
 			</div>
 		)
 	}
