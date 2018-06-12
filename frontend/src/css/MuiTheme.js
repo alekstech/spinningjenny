af6756b54b9e14d91bed216964d2815ebca7e222
Aleks Sobieraj <contact@aleks.tech>
2018-06-11 23:26:00 -0400

    responsive font size formula

--- frontend/src/css/MuiTheme.js
+++ frontend/src/css/MuiTheme.js
@@ -1,7 +1,9 @@
 import { createMuiTheme } from 'material-ui/styles'
 
 const theme = createMuiTheme({
-	'typography': {}
+	'typography': {
+		htmlFontSize: 'calc(16px + 6 * ((100vw - 320px) / 1200))'
+	}
 })
 
 export default theme
